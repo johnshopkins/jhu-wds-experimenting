f169197a365a779f4a3d54ea017ecc9af4a279e7
Jen Wachter <jwachter@jhu.edu>
2024-05-24 13:48:50 -0400

    setup viewports and modes based on our breakpoints

---
+++ .storybook/modes.js
@@ -0,0 +1,7 @@
+// see .storybook/preview for viewport definitions
+export const allModes = {
+  hand: { viewport: "hand" },
+  desk: { viewport: "desk" },
+  wall: { viewport: "wall" },
+  jumbotron: { viewport: "jumbotron" },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -19,6 +19,42 @@ const preview = {
         ],
       },
     },
+    /**
+     * I'm using our breakpoints for now, but we can also add specific devices:
+     * https://storybook.js.org/docs/essentials/viewport#use-a-detailed-set-of-devices
+     */
+    viewport: {
+      viewports: {
+        hand: {
+          name: "Hand",
+          styles: {
+            width: "576px",
+            height: "384px"
+          }
+        },
+        desk: {
+          name: "Desk",
+          styles: {
+            width: "864px",
+            height: "576px"
+          }
+        },
+        wall: {
+          name: "Wall",
+          styles: {
+            width: "1296px",
+            height: "768px"
+          }
+        },
+        jumbotron: {
+          name: "Jumbotron",
+          styles: {
+            width: "1728px",
+            height: "960px"
+          }
+        },
+      }
+    },
   },
 };
 
