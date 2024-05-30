313d1f84e0e590bfcd196448cbb875cde54d11ae
Jen Wachter <jwachter@jhu.edu>
2024-05-30 15:25:19 -0400

    Added basic theme

---
+++ .storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+	base: 'light',
+  brandTitle: 'Johns Hopkins University Web Design System',
+  brandImage: 'https://www.jhu.edu/assets/themes/machado/src/assets/images/logos/university-logo-small-horizontal-black.svg',
+
+  colorPrimary: '#002D72',
+  colorSecondary: '#FF9E1B',
+});
