1395bf355220deb43f360f6368688e565ff0e281
Jen Wachter <jwachter@jhu.edu>
2024-05-30 09:23:15 -0400

    build library using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
+    globalObject: 'this',
     library: {
       name: 'jhuwds',
       type: 'umd',
