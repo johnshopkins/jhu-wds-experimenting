e27aa9309b8b962ae1cfbf17c4b14a164a867822
Jen Wachter <jwachter@jhu.edu>
2024-05-30 15:28:15 -0400

    fixed indentation

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,10 +1,7 @@
 import { create } from '@storybook/theming/create';
 
 export default create({
-	base: 'light',
+  base: 'light',
   brandTitle: 'Johns Hopkins University Web Design System',
   brandImage: 'https://www.jhu.edu/assets/themes/machado/src/assets/images/logos/university-logo-small-horizontal-black.svg',
-
-  colorPrimary: '#002D72',
-  colorSecondary: '#FF9E1B',
 });
